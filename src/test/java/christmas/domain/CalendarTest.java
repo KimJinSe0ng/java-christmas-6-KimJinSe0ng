d6dfef23cfcabbdc856a9420a8559ef77e9cbcca
BigBird <waujoxc@gmail.com>
2023-11-15 16:52:54 +0900

    test(calendar): 평일에 해당하는 날짜를 입력했을 때 평일인지 검증

---
+++ src/test/java/christmas/domain/CalendarTest.java
@@ -0,0 +1,22 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalendarTest {
+
+    @DisplayName("평일에 해당하는 날짜를 입력했을 때 검증")
+    @Test
+    public void testIsWeekday() {
+        // Given
+        int weekday = 5;
+
+        // When
+        boolean result = Calendar.isWeekday(weekday);
+
+        // Then
+        assertTrue(result, "평일이 맞음");
+    }
+}
