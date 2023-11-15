05e7f968dbc0bf5d22234b0eade5c3e9c52f6dc4
BigBird <waujoxc@gmail.com>
2023-11-15 17:19:01 +0900

    test(result): 할인 전 총주문 금액으로 증정 메뉴 검증

--- src/test/java/christmas/domain/ResultTest.java
+++ src/test/java/christmas/domain/ResultTest.java
@@ -25,4 +25,21 @@ class ResultTest {
         // Then
         assertEquals(125000, result.getTotalBeforeDiscount());
     }
+
+    @DisplayName("할인 전 총주문 금액으로 증정 메뉴 검증")
+    @Test
+    public void testCalculateServiceMenuWithDiscount() {
+        // Given
+        Map<Menu, Integer> orderedItems = new HashMap<>();
+        orderedItems.put(Menu.티본스테이크, 2);
+        orderedItems.put(Menu.초코케이크, 1);
+
+        int date = 20; // Any date
+
+        // When
+        Result result = Result.from(orderedItems, date);
+
+        // Then
+        assertEquals(1, result.getServiceMenu());
+    }
 }
