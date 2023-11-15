0096b7a1244233ab49fc0b4d6daa11eb582825f1
BigBird <waujoxc@gmail.com>
2023-11-15 17:32:32 +0900

    test(validator): 날짜 문자열을 숫자로 바꾸는 테스트로 유효하지 않은 포맷을 입력했을 때 검증

--- src/test/java/christmas/validator/ValidatorTest.java
+++ src/test/java/christmas/validator/ValidatorTest.java
@@ -2,6 +2,8 @@ package christmas.validator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import christmas.exception.ErrorMessage;
+import christmas.exception.ValidatorException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,4 +22,15 @@ class ValidatorTest {
         assertEquals(20, result);
     }
 
+    @DisplayName("날짜 문자열을 숫자로 바꾸는 테스트로 유효하지 않은 포맷을 입력했을 때 검증")
+    @Test
+    public void testConvertDateStringToInt_InvalidFormat() {
+        // Given
+        String input = "abc";
+
+        // When/Then
+        ValidatorException exception = assertThrows(ValidatorException.class,
+                () -> Validator.convertDateStringToInt(input));
+        assertEquals(ErrorMessage.INVALID_DATE.getMessage(), exception.getMessage());
+    }
 }
